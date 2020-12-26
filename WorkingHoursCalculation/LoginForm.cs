c68898cc92defcf845d4d623e6dd0e0b7c24dc21
cuibin <sdcuibin@outlook.com>
2020-12-26 17:07:28 +0800

    访问数据库操作类修改

--- WorkingHoursCalculation/LoginForm.cs
+++ WorkingHoursCalculation/LoginForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using WorkingHoursCalculation.Helpers;
 
 namespace WorkingHoursCalculation
 {
@@ -44,7 +45,7 @@ namespace WorkingHoursCalculation
         /// <param name="e"></param>
         private void btn_Login_Click(object sender, EventArgs e)
         {
-
+            DataTable dt = DbHelperOleDb.Query().Tables[0];
         }
 
         /// <summary>
